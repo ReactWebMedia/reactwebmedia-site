e63eb8b1aeab72f09544789cb8bea8d101d29117
Jean Lescure <jean@ticowebmedia.com>
2016-01-18 08:14:18 +0000

    Paypal footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -256,10 +256,27 @@ class HomePage extends React.Component {
                 <button type="submit" className="btn btn-default">Get Sarted</button>
               </form>
             </div>
-            <div className="hidden-xs col-sm-3">
-              <div className="footer-logo-holder">
-                <img src="/images/reactwebmedia-logo-icon.png" alt=""/>
-              </div>
+            <div className="hidden-xs col-sm-3 footer-paypal">
+              <table border="0" cellPadding="10" cellSpacing="0" align="right">
+                <tbody>
+                  <tr>
+                    <td align="center"></td>
+                  </tr>
+                  <tr>
+                    <td align="center">
+                      <a href="https://www.paypal.com/cr/webapps/mpp/what-is-paypal" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/cr/webapps/mpp/what-is-paypal','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;">
+                        <img src="https://www.paypalobjects.com/webstatic/mktg/logo/bdg_secured_by_pp_2line.png" border="0" alt="Secured by PayPal"/>
+                      </a>
+                      <div className="center">
+                        <br/>
+                        <a href="https://www.paypal.com/cr/webapps/mpp/what-is-paypal" face="Arial" color="#0079CD">
+                          How PayPal Works
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
             <div className="hidden-xs col-sm-2">
               &nbsp;
